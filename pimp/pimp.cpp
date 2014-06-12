da85348af3ce5e41a1eb7f5e31a9a7522fd00fd4
Sam Hocevar <sam@hocevar.net>
2014-06-12 12:13:47 +0000

    image: add methods to save images and a small test program.

---
+++ pimp/pimp.cpp
@@ -0,0 +1,34 @@
+//
+//  Pimp - Pathetic Image Manipulation Program
+//
+//  Copyright (c) 2008-2014 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+//
+
+#if defined HAVE_CONFIG_H
+#   include "config.h"
+#endif
+
+#include "core.h"
+
+using namespace lol;
+
+int main(int argc, char **argv)
+{
+    UNUSED(argc, argv);
+
+    Image *image = Image::Create("input.jpeg");
+
+    ivec2 size = image->GetSize();
+    uint8_t *data = image->GetData();
+
+    image->Save("output.bmp");
+
+    Image::Destroy(image);
+
+    return 0;
+}
+
