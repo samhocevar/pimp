97d2791f72e36e0d04d0a16b20cfcb595b625c70
Sam Hocevar <sam@hocevar.net>
2014-06-17 15:14:26 +0000

    image: port some error diffusion dithering algorithms.

--- pimp/pimp.cpp
+++ pimp/pimp.cpp
@@ -31,19 +31,28 @@ int main(int argc, char **argv)
     u8vec4 *data = other.Lock<PixelFormat::RGBA_8>();
     for (int n = 0; n < size.x * size.y; ++n)
     {
+        /* Swap R and B */
         data[n] = data[n].bgra;
+
+        /* Reduce red value */
+        data[n].r *= 0.9f;
     }
     other.Unlock(data);
-
-    other = other.AutoContrast();
     other.Save("output1.jpeg");
+
+    /* Dither to black and white */
+    //Image kernel;
+    //kernel.Stock("ediff:jajuni");
+    //other = other.DitherEdiff(kernel, ScanMode::Serpentine);
+    other = other.DitherOstromoukhov(ScanMode::Serpentine);
+    other.Save("output2.jpeg");
 
     /* More tests */
     Image tmp;
     tmp.RenderRandom(ivec2(256, 256));
     // FIXME: this should not be necessary
     tmp.SetFormat(PixelFormat::RGBA_F32);
-    tmp.Save("output2.jpeg");
+    tmp.Save("output3.jpeg");
 
     return 0;
 }
