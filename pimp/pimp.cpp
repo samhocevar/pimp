b2c5a61c1162531846c85a70350041234e69895f
Sam Hocevar <sam@hocevar.net>
2014-06-20 11:34:42 +0000

    image: add Convolution() method, with optimisation for separable filters.

--- pimp/pimp.cpp
+++ pimp/pimp.cpp
@@ -16,14 +16,8 @@
 
 using namespace lol;
 
-int main(int argc, char **argv)
+void DoSomeSeriousShit(Image &image)
 {
-    UNUSED(argc, argv);
-
-    /* Load image */
-    Image image;
-    image.Load("input.jpeg");
-
     /* Copy image to another variable */
     Image other = image;
     ivec2 size = other.GetSize();
@@ -38,6 +32,14 @@ int main(int argc, char **argv)
         data[n].r *= 0.9f;
     }
     other.Unlock(data);
+
+    /* Convolution */
+    Array2D<float> kernel(3, 3);
+    kernel[0][0] = -3; kernel[1][0] = -4; kernel[2][0] = -3;
+    kernel[0][1] = 0;  kernel[1][1] = 0;  kernel[2][1] = 0;
+    kernel[0][2] = 3;  kernel[1][2] = 4;  kernel[2][2] = 3;
+    other = other.Convolution(kernel);
+
     other.Save("output1.jpeg");
 
     /* Dither to black and white */
@@ -47,12 +49,46 @@ int main(int argc, char **argv)
     other = other.DitherOstromoukhov(ScanMode::Serpentine);
     other.Save("output2.jpeg");
 
+#if 0
+    /* Convert to braille! */
+    {
+        float *pixels = other.Lock<PixelFormat::Y_F32>();
+        for (int y = 0; y < size.y; y += 4)
+        {
+            for (int x = 0; x < size.x; x += 2)
+            {
+                int ch = 0;
+                ch += (int)(pixels[y * size.x + x] < 0.5f) << 0;
+                ch += (int)(pixels[(y + 1) * size.x + x] < 0.5f) << 1;
+                ch += (int)(pixels[(y + 2) * size.x + x] < 0.5f) << 2;
+                ch += (int)(pixels[y * size.x + x + 1] < 0.5f) << 3;
+                ch += (int)(pixels[(y + 1) * size.x + x + 1] < 0.5f) << 4;
+                ch += (int)(pixels[(y + 2) * size.x + x + 1] < 0.5f) << 5;
+                ch += (int)(pixels[(y + 3) * size.x + x] < 0.5f) << 6;
+                ch += (int)(pixels[(y + 3) * size.x + x + 1] < 0.5f) << 7;
+                printf("%c%c%c", 0xe2, 0xa0 + (ch >> 6), 0x80 + (ch & 0x3f));
+            }
+            printf("\n");
+        }
+        other.Unlock(pixels);
+    }
+#endif
+
     /* More tests */
     Image tmp;
     tmp.RenderRandom(ivec2(256, 256));
     // FIXME: this should not be necessary
     tmp.SetFormat(PixelFormat::RGBA_F32);
     tmp.Save("output3.jpeg");
+}
+
+int main(int argc, char **argv)
+{
+    UNUSED(argc, argv);
+
+    Image image;
+    image.Load("input.jpeg");
+    DoSomeSeriousShit(image);
 
     return 0;
 }
