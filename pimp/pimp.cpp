fdbc2216ce72c38bc1615773b2a109dbfb9b4aa4
Sam Hocevar <sam@hocevar.net>
2014-07-07 18:14:16 +0000

    pimp: add some clustering in the Amiga export to speed up things.

--- pimp/pimp.cpp
+++ pimp/pimp.cpp
@@ -19,11 +19,16 @@ using namespace lol;
 
 Image EHBConvert(Image src)
 {
+    ivec2 size = src.GetSize();
+    Image dst(size);
+
+    array2d<vec4> &srcdata = src.Lock2D<PixelFormat::RGBA_F32>();
+
     int const COLORS = 32;
     int const RECYCLED_COLORS = 16;
-    int const FIXED_COLORS = 12;
-
-    static_assert(RECYCLED_COLORS + FIXED_COLORS < COLORS, "no free colors!");
+    int const FIXED_COLORS = 8;
+    int const FREE_COLORS = RECYCLED_COLORS - FIXED_COLORS;
+    static_assert(FREE_COLORS > 0, "no free colors!");
 
     /* Build the Amiga colour palette */
     array<vec3> hipal, lopal;
@@ -34,13 +39,47 @@ Image EHBConvert(Image src)
         lopal.Push(vec3(col / 2) / 15.f);
     }
 
-    ivec2 size = src.GetSize();
-    Image dst(size);
+    /* Build clusters of pixels so that we know which colours are
+     * important or not. */
+    array2d<int> pixelcount(size);
+    memset(pixelcount.Data(), 0, pixelcount.Bytes());
+    for (int y = 0; y < size.y; ++y)
+        for (int x = 0; x < size.x; ++x)
+        {
+            if (pixelcount[x][y])
+                continue;
+            ivec3 p = ivec3(srcdata[x][y].rgb * 255.999f);
 
-    array2d<vec4> &srcdata = src.Lock2D<PixelFormat::RGBA_F32>();
+            array<ivec2> visited, unvisited;
+            unvisited.Push(ivec2(x, y));
+
+            while (unvisited.Count())
+            {
+                ivec2 m = unvisited.Pop();
+                visited.Push(m);
+
+                for (ivec2 d : { ivec2(-1, 0), ivec2(1, 0),
+                                 ivec2(0, -1), ivec2(0, 1) })
+                {
+                    if (visited.Find(m + d) != -1)
+                        continue;
+                    if (m.x + d.x < 0 || m.x + d.x >= size.x
+                         || m.y + d.y < 0 || m.y + d.y >= size.y)
+                        continue;
+
+                    ivec3 q = ivec3(srcdata[m.x + d.x][m.y + d.y].rgb * 255.999f);
+                    if (p == q)
+                        unvisited.Push(m + d);
+                }
+            }
+
+            for (ivec2 coord : visited)
+                pixelcount[coord] = visited.Count();
+        }
 
 #if 1
-    /* Convert source image to Amiga colours */
+    /* Convert source image to Amiga colours -- do this if working on
+     * pixel art rather than photographs. */
     for (int y = 0; y < size.y; ++y)
         for (int x = 0; x < size.x; ++x)
         {
@@ -55,10 +94,7 @@ Image EHBConvert(Image src)
     /* Treat our image, line by line. */
     for (int y = 0; y < size.y; ++y)
     {
-        array<int> super_palette;
-        float super_msd = (float)size.x;
-
-        /* Make a histogram of the line */
+        /* Count how important the pixels of the line are. */
         int histo[4096] = { 0 };
         for (int x = 0; x < size.x; ++x)
         {
@@ -71,22 +107,7 @@ Image EHBConvert(Image src)
             if (p < ivec3(8))
                 index *= 2;
 
-            ++histo[index];
-
-            /* Count colours from adjacent lines, but only if they appear
-             * in the current line. This tells us which colours are a bit
-             * more important than the others. */
-            for (int dy : { -3, -2, -1, 0, 1, 2, 3 })
-            for (int dx : { -3, -2, -1, 0, 1, 2, 3 })
-            {
-                if (y + dy < 0 || y + dy >= size.y
-                     || x + dx < 0 || x + dx >= size.x)
-                    continue;
-
-                ivec3 q = ivec3(vec3(srcdata[x + dx][y + dy].rgb) * 15.999f);
-                if (p == q)
-                    ++histo[index];
-            }
+            histo[index] += pixelcount[ivec2(x, y)];
         }
 
         /* Count number of colours in this line */
@@ -94,7 +115,9 @@ Image EHBConvert(Image src)
         for (int i = 0; i < 4096; ++i)
             count += histo[i] ? 1 : 0;
 
+#if 1
         printf("line %d: %d colors\n", y, count);
+#endif
 
         /* Sort palette */
         array<int> sorted_palette;
@@ -110,34 +133,93 @@ Image EHBConvert(Image src)
 
         /* Recycle up to RECYCLED_COLORS from last line */
         array<int> recycled_palette;
-        for (auto c : sorted_palette)
+        for (int i = 0; i < sorted_palette.Count(); )
         {
             if (recycled_palette.Count() >= RECYCLED_COLORS)
                 break;
-            if (prev_line_palette.Find(c) == -1)
-                continue;
-            recycled_palette << c;
+            if (prev_line_palette.Find(sorted_palette[i]) == -1)
+            {
+                ++i;
+            }
+            else
+            {
+                recycled_palette << sorted_palette[i];
+                sorted_palette.Remove(i);
+            }
         }
 
-        /* Pad recycled palette with stuff from prev line. */
-        for (auto c : prev_line_palette)
+        /* Pad recycled palette. */
+        while (recycled_palette.Count() < RECYCLED_COLORS)
         {
-            if (recycled_palette.Count() >= RECYCLED_COLORS)
-                break;
-            if (recycled_palette.Find(c) != -1)
-                continue;
-            recycled_palette << c;
+            if (prev_line_palette.Count())
+                recycled_palette.PushUnique(prev_line_palette.Pop());
+            else
+                recycled_palette.PushUnique(rand(4096));
         }
 
-        /* Fix FIXED_COLORS colors for this line */
+        /* Fix up to FIXED_COLORS colors for this line. We just pick
+         * the most important ones from sorted_palette. */
         array<int> fixed_palette;
-        for (auto c : sorted_palette)
+        while (sorted_palette.Count()
+                && fixed_palette.Count() < FIXED_COLORS)
         {
-            if (fixed_palette.Count() >= FIXED_COLORS)
-                break;
-            if (recycled_palette.Find(c) != -1)
-                continue;
-            recycled_palette << c;
+            fixed_palette.PushUnique(sorted_palette[0]);
+            sorted_palette.Remove(0);
+        }
+
+        while (fixed_palette.Count() < FIXED_COLORS)
+            fixed_palette.PushUnique(rand(4096));
+
+        /* The rest are free colours; if we have enough room for the
+         * last colours we need, use these. Otherwise, we need to
+         * search for the best combination. */
+        array<int> free_palette;
+        if (sorted_palette.Count() <= FREE_COLORS)
+        {
+            free_palette += sorted_palette;
+            while (free_palette.Count() < FREE_COLORS)
+                free_palette.PushUnique(rand(4096));
+        }
+        else
+        {
+            float bestscore = (float)size.x;
+
+            for (int iter = 0; iter < 2000; ++iter)
+            {
+                /* Pick FREE_COLORS colours at random */
+                array<int> test_palette;
+                while (test_palette.Count() < FREE_COLORS)
+                {
+                    int index = rand(sorted_palette.Count());
+                    test_palette.PushUnique(sorted_palette[index]);
+                }
+
+                array<int> palette = recycled_palette
+                                          + fixed_palette + test_palette;
+                /* Compute error */
+                float error = 0.0f;
+                for (int x = 0; x < size.x; ++x)
+                {
+                    vec3 p = srcdata[x][y].rgb;
+                    float best_dist = 1.0f;
+
+                    for (int c = 0; c < COLORS; ++c)
+                    {
+                        best_dist = min(best_dist,
+                                        sqlength(p - hipal[palette[c]]));
+                        best_dist = min(best_dist,
+                                        sqlength(p - lopal[palette[c]]));
+                    }
+
+                    error += best_dist;
+                }
+
+                if (error < bestscore)
+                {
+                    bestscore = error;
+                    free_palette = test_palette;
+                }
+            }
         }
 
 #if 0
@@ -147,76 +229,9 @@ Image EHBConvert(Image src)
         printf("\n");
 #endif
 
-        for (int iter = 0; iter < 50; ++iter)
-        {
-            array<int> palette = recycled_palette + fixed_palette;
-            palette.Resize(COLORS);
-
-            /* Generate random palette */
-            for (int c = RECYCLED_COLORS + FIXED_COLORS; c < COLORS; ++c)
-                palette[c] = rand(4096);
-
-            float best_msd = (float)size.x;
-            int changed_index = 0;
-            int changed_color = palette[0];
-
-            for (int failures = 0; failures < 50; )
-            {
-                /* Dither one line */
-                float msd = 0.0f;
-                for (int x = 0; x < size.x; ++x)
-                {
-                    vec3 pixel = srcdata[x][y].rgb;
-                    float dist_squared = 1.0f;
-
-                    for (int c = 0; c < COLORS; ++c)
-                    {
-                        dist_squared = min(dist_squared,
-                                           sqlength(hipal[palette[c]] - pixel));
-                        dist_squared = min(dist_squared, /* EHB */
-                                           sqlength(lopal[palette[c]] - pixel));
-                    }
-
-                    if (dist_squared < 0.5f / (16 * 16))
-                        dist_squared = 0.f;
-                    msd += dist_squared;
-                }
-
-                if (msd >= best_msd)
-                {
-                    /* If the result wasn't good, revert. */
-                    ++failures;
-
-                    palette[changed_index] = changed_color;
-                }
-                else
-                {
-                    failures = 0;
-
-                    /* Otherwise, commit */
-                    best_msd = msd;
-                }
-
-                /* Change one color */
-                changed_index = rand(RECYCLED_COLORS + FIXED_COLORS, COLORS);
-                changed_color = palette[changed_index];
-                int new_color = changed_color;
-                new_color += rand(-1, 2);
-                new_color += rand(-1, 2) * 16;
-                new_color += rand(-1, 2) * 256;
-                palette[changed_index] = clamp(new_color, 0, 4095);
-
-                if (best_msd > 5.f * super_msd)
-                    break;
-            }
-
-            if (best_msd < super_msd)
-            {
-                super_msd = best_msd;
-                super_palette = palette;
-                printf("[%d] .. %f\n", y, best_msd);
-            }
-        }
+        array<int> palette = recycled_palette + fixed_palette + free_palette;
+        ASSERT(palette.Count() == COLORS,
+               "only %d colours in palette", palette.Count());
 
         /* Commit the best palette */
         array2d<vec4> &dstdata = dst.Lock2D<PixelFormat::RGBA_F32>();
@@ -229,8 +244,8 @@ Image EHBConvert(Image src)
 
             for (int c = 0; c < COLORS * 2 /* EHB */; ++c)
             {
-                vec3 color = c < COLORS ? hipal[super_palette[c]]
-                                        : lopal[super_palette[c - COLORS]];
+                vec3 color = c < COLORS ? hipal[palette[c]]
+                                        : lopal[palette[c - COLORS]];
                 float dist = sqlength(pixel - color);
                 if (dist < best_dist)
                 {
@@ -259,11 +274,6 @@ Image EHBConvert(Image src)
         }
 
         dst.Unlock2D(dstdata);
-
-        if (y % 10 == 9)
-            dst.Save("ehb.png");
-
-        printf("[%d] FINAL %f\n", y, super_msd);
     }
 
     src.Unlock2D(srcdata);
