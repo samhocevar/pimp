c04856ce004621eb13c5a36a68db6761c2ec96aa
Sam Hocevar <sam@hocevar.net>
2015-02-11 18:59:36 +0000

    misc: lots of compilation fixes for Visual Studio (related to mediocre support for enable_if and other template metaprogramming techniques), for Clang (related to static const template class member initialisation) and for GCC (related to forward declaration of non-int enum classes).

--- pimp/pimp.cpp
+++ pimp/pimp.cpp
@@ -1,11 +1,13 @@
 //
 //  Pimp - Pathetic Image Manipulation Program
 //
-//  Copyright (c) 2008-2014 Sam Hocevar <sam@hocevar.net>
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of the Do What The Fuck You Want To
-//   Public License, Version 2, as published by Sam Hocevar. See
-//   http://www.wtfpl.net/ for more details.
+//  Copyright © 2008-2015 Sam Hocevar <sam@hocevar.net>
+//
+//  This program is free software. It comes without any warranty, to
+//  the extent permitted by applicable law. You can redistribute it
+//  and/or modify it under the terms of the Do What the Fuck You Want
+//  to Public License, Version 2, as published by the WTFPL Task Force.
+//  See http://www.wtfpl.net/ for more details.
 //
 
 #if HAVE_CONFIG_H
@@ -311,7 +313,7 @@ int main(int argc, char **argv)
         im1.Unlock2D(im1data);
         im2.Unlock2D(im2data);
 
-        printf("mean distance = %f\n", sqrt(delta / (size.x * size.y)));
+        printf("mean distance = %f\n", lol::sqrt(delta / (size.x * size.y)));
     }
 
 #if 0
